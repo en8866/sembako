9132e7a61104fee4a40505e6775ff71b7ebc3570
en8866 <e82821166@gmail.com>
2025-04-10 16:12:35 +0700

    fix: fix route

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\SembakoController;
 
 Route::get('/sembako', [SembakoController::class, 'index'])->name('sembako.index');
 Route::get('/sembako/create', [SembakoController::class, 'create'])->name('sembako.create');
-Route::get('/sembako', [SembakoController::class, 'index'])->name('sembako.index');
 Route::post('/sembako', [SembakoController::class, 'store'])->name('sembako.store');
