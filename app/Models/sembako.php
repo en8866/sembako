7fcecbe85b44280430bf9c5cd39257a67e58aeba
en8866 <e82821166@gmail.com>
2025-04-15 14:18:54 +0700

    fix: membuat CRUD #3

--- app/Models/sembako.php
+++ app/Models/sembako.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Sembako extends Model
 {
+    protected $table = 'sembako'; 
+
     protected $fillable = ['nama_barang', 'jumlah_barang', 'harga_barang', 'satuan'];
 }
